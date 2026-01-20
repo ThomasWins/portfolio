ddfc8ee98c46ffac0bd8d29861527966779562b4
Thomas Winslow <twinslow128@gmail.com>
2026-01-19 21:18:20 -0500

    Updated website to include projects, resume and correct contact info. Also added privacy, terms, and fixed contact footer

--- src/main/java/in/thomaswins/portfolio/controller.java
+++ src/main/java/in/thomaswins/portfolio/controller.java
@@ -1,16 +1,46 @@
-package in.thomaswins.portfolio.controller;
+package in.thomaswins.portfolio;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class controller {
 
     @GetMapping({"/", "", "/home"})
-    public String showHomePage() {
-        return "home";
+    public String showHomePage(Model model) {
+        model.addAttribute("title", "Home");
+        return "master";
     }
 
-    
+    @GetMapping("/projects")
+    public String showProjectsPage(Model model) {
+        model.addAttribute("title", "Projects");
+        return "master";
+    }
+
+    @GetMapping("/resume")
+    public String showResumePage(Model model) {
+        model.addAttribute("title", "Resume");
+        return "master";
+    }
+
+    @GetMapping("/contact")
+    public String showContactPage(Model model) {
+        model.addAttribute("title", "Contact");
+        return "master";
+    }
+
+    @GetMapping("/privacy")
+    public String showPrivacyPage(Model model) {
+        model.addAttribute("title", "Privacy");
+        return "master";
+    }
+
+    @GetMapping("/terms")
+    public String showTermsPage(Model model) {
+        model.addAttribute("title", "Terms");
+        return "master";
+    }
 
 }
